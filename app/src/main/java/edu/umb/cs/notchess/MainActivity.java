802887308db81bedc1bdbab7a9f65cf8f5970910
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2021-05-20 11:55:51 -0400

    add version name and signature to the main activity

--- app/src/main/java/edu/umb/cs/notchess/MainActivity.java
+++ app/src/main/java/edu/umb/cs/notchess/MainActivity.java
@@ -1,9 +1,13 @@
 package edu.umb.cs.notchess;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            TextView versionName = findViewById(R.id.versionName);
+            versionName.setText(pInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onClickStartGame(View view) {
